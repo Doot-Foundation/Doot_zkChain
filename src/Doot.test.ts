b37a43784e9895c41208a36b1e8176e1f958c1a3
br0wnD3v <bothereum@protonmail.com>
2023-10-20 05:33:17 +0530

    first commit

---
+++ src/Doot.test.ts
@@ -0,0 +1,40 @@
+import { TestingAppChain } from "@proto-kit/sdk";
+import { PrivateKey, PublicKey } from "snarkyjs";
+import { Doot } from "./Doot";
+import { log } from "@proto-kit/common";
+
+log.setLevel("ERROR");
+
+describe("Doot", () => {
+  it("Should initialize the oracle public key.", async () => {
+    const appChain = TestingAppChain.fromRuntime({
+      modules: {
+        Doot,
+      },
+      config: {
+        Doot: {},
+      },
+    });
+
+    await appChain.start();
+
+    const alicePrivateKey = PrivateKey.random();
+    const alice = alicePrivateKey.toPublicKey();
+
+    appChain.setSigner(alicePrivateKey);
+
+    const doot = appChain.runtime.resolve("Doot");
+
+    const tx1 = appChain.transaction(alice, () => {
+      doot.setOracle(alice);
+    });
+
+    await tx1.sign();
+    await tx1.send();
+
+    await appChain.produceBlock();
+
+    const key = await appChain.query.runtime.Doot.oracleKey.get();
+    console.log(key);
+  });
+});
---
+++ src/Doot.ts
@@ -0,0 +1,32 @@
+import {
+  RuntimeModule,
+  runtimeModule,
+  state,
+  runtimeMethod,
+} from "@proto-kit/module";
+
+import { State } from "@proto-kit/protocol";
+import { CircuitString, Field, PublicKey, Signature } from "snarkyjs";
+
+interface DootConfig {}
+
+@runtimeModule()
+export class Doot extends RuntimeModule<DootConfig> {
+  @state() public price = State.from<Field>(Field);
+  @state() public decimals = State.from<Field>(Field);
+  @state() public timestamp = State.from<Field>(Field);
+  @state() public signature = State.from<Signature>(Signature);
+  @state() public tokenPair = State.from<CircuitString>(CircuitString);
+  @state() public oracleKey = State.from<PublicKey>(PublicKey);
+
+  @runtimeMethod()
+  public updateInformation(
+    _price: Field,
+    _decimals: Field,
+    _timestamp: Field
+  ) {}
+
+  @runtimeMethod() public setOracle(_address: PublicKey) {
+    this.oracleKey.set(_address);
+  }
+}
